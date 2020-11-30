770a5482f8fc3c4fddcd6a97103cecf27c93e7ec
DevOM oplondhe@gmail.com <oplondhe@gmail.com>
2020-11-30 20:26:46 +0530

    first commit

---
+++ src/SongRow.js
@@ -0,0 +1,16 @@
+import './SongRow.css';
+import React from 'react';
+
+const SongRow = ({ track }) => {
+    return (
+        <div className="songRow">
+            <img className="songRow__album" src={track.album.images[0].url} alt="" />
+            <div className="songRow__info">
+                <h1>{track.name}</h1>
+                <p>{track.artists.map(aetist => artist.name).join(", ")}-{" "}{track.album.name}</p>
+            </div>
+        </div>
+    )
+}
+
+export default SongRow;
